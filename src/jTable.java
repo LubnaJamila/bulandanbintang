a6b63f7eeef2ff2578f9b63cd0afc4d5100a19ba
LubnaJamila <lubnajamila024@gmail.com>
2023-08-28 20:30:25 +0700

    First Upload

---
+++ src/jTable.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author asus
+ */
+public class jTable {
+    public static void main(String[] args) {
+        barang A = new barang();
+        A.setVisible(true);
+        
+    }
+    
+}
